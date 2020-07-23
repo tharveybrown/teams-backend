23f3f46620da0525235c51e57ca269e074fe48ab
tharveybrown <tharveybrown@gmail.com>
2020-07-23 16:30:41 -0700

    bot gem, send messages, yeet

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -11,11 +11,18 @@ class SessionsController < ApplicationController
     res = Net::HTTP.get_response(uri)
     # @employee = Employee.find_or_create_by(full_name: auth_hash.info.name, member_id: auth_hash.info.user_id)
     response = JSON.parse(res.body)
-    access_token = response['access_token']
+    bot_access_token = response['access_token']
+    access_token = response['authed_user']['access_token']
+    con = Faraday.new
+    resp = con.post do |req| 
+      req.url "https://slack.com/api/chat.postMessage" 
+      req.headers['Authorization'] = "Bearer #{bot_access_token}" 
+      req.body = '{ "channel": "CT8DCFL02", "text": "hiii" }' 
+    end
+    byebug
     @employee = Employee.find(session_user[:id])
     @employee.access_token = access_token
     @employee.save
-    
     slack_team = SlackTeam.find_or_create_by(slack_id: response['team']['id'], name: response['team']['name'], organization_id: session_user.organization.id)
     channels = slack_team.fetch_channels(access_token)['channels'].map{|channel| {name: channel['name'], id: channel['id'], is_private: channel['is_private']}}
     users = slack_team.users(access_token)
@@ -27,7 +34,6 @@ class SessionsController < ApplicationController
     slack_team = session_user.slack_team
     users = slack_team.users(session_user.access_token)
     users = slack_team.users(session_user['access_token'])
-    
     slack_users = users.map{|user| {email: user['profile']['email'], id: user['id'], image: user['profile']['image_72'] , is_admin: user['profile']['is_admin'], slack_team_id: user['team_id'], is_employee: !!Employee.find_by(email:user['profile']['email'] )}}
     channels = slack_team.fetch_channels(session_user['access_token'])['channels'].map{|channel| {name: channel['name'], id: channel['id'], is_private: channel['is_private']}}
     render json: {slack: slack_team, slack_users: slack_users, channels: channels}
