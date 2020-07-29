0be8004ae91c720574298215225b037d84f7bf06
tharveybrown <tharveybrown@gmail.com>
2020-07-29 11:37:37 -0700

    fetch first

--- app/controllers/channels_controller.rb
+++ app/controllers/channels_controller.rb
@@ -4,7 +4,7 @@ class ChannelsController < ApplicationController
 
   def top_channels
     slack_team = session_user.slack_team
-    channels = slack_team.fetch_channels(slack_team.bot_token)
+    # channels = slack_team.fetch_channels(slack_team.bot_token)
     top_channels = slack_team.channels.order('num_members DESC').limit(10)
     # byebug
     top_channel_messages = top_channels.map{|channel| {"channel" => channel, "messages"=> channel.messages.first ? true : false}}
@@ -34,7 +34,7 @@ class ChannelsController < ApplicationController
   
   def index
     slack_team = session_user.slack_team
-    # channels = slack_team.fetch_channels(slack_team.bot_token)
+    channels = slack_team.fetch_channels(slack_team.bot_token)
     channels = slack_team.channels
     if channels 
       render json: {channels: channels}
