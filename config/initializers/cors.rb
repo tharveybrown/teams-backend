97b24f58653a11cf2462c89114986139b3bdc701
tharveybrown <tharveybrown@gmail.com>
2020-07-30 14:26:08 -0700

    https update

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://localhost:3000', 'http://localhost:3000' ,  'https://murmuring-escarpment-97300.herokuapp.com', 'http://www.webbed.us'
+    origins 'https://localhost:3000', 'http://localhost:3000' ,  'https://murmuring-escarpment-97300.herokuapp.com', 'https://www.webbed.us'
   # 
     resource '*',
       headers: :any,
