ee1058c855c33ac127183241615dc64f5962d45e
tharveybrown <tharveybrown@gmail.com>
2020-08-03 09:50:42 -0600

    readme

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module TeamsBackend
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.active_record.belongs_to_required_by_default = false
-    config.force_ssl = true
+    # config.force_ssl = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
